976b5d2fa447a8055d7ec75056988d95f0524c07
58691_Raman <ramanmenche@gmail.com>
2023-06-14 18:25:02 +0530

    updated

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
 #Establish a connection to the SQL server
 from flask import Flask, jsonify, request, render_template
+import sqlite3
+conn = sqlite3.connect('ims.db')
 app = Flask(__name__)
-import pyodbc
 
-server = 'MANCHIKATLA\SQLEXPRESS'
-database = 'NEW_IMS'
-driver = '{SQL Server}'
+def idgenerator(tab):
+    conn = sqlite3.connect('ims.db')
+    cn = conn.cursor()
+    idval = ''
+    if tab=='CUSTOMER':
+        idval = 'CUSTOMER_ID'
+    if tab=='PRODUCT':
+        idval = 'PRODUCT_ID'
+    if tab=='ORDERS':
+        idval = 'ORDER_ID'
+    if tab=='SUPPLIER':
+        idval = 'SUPPLIER_ID'
+    print(tab,idval)
+    cn.execute(f"SELECT {idval} FROM {tab}")
+    new = cn.fetchall()
+    cud = str(new[len(new)-1][0])
+    for i in range(len(str(cud))):
+        if cud[i].isnumeric():
+            f = i
+            break
+    myint = cud[f:]
+    myint = int(myint)+1
+    return idval[0:3]+str(myint)
 
-connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};trusted_connection=yes'
-
-conn = pyodbc.connect(connection_string)
-
-# cn = conn.cursor()
-
-# cn.execute("SELECT * FROM CUSTOMER")
-#print(cn.fetchall())
-#using the variables the data has been inserted by using print(f"{}{}")
-# customer_name = 'chotu'
-# customer_address = 'sangareddy'
-# customer_email = 'chotu@gmail.com'
-#inserting the data into the customer table....
-#cn.execute("insert into customer(customer_name.customer_address,customer_email) values ('srikanth','nagaram','srikanth@gmail.com')")
-'''
-cn.execute(f"insert into customer(customer_name,customer_address,customer_email) values ('{customer_name}','{customer_address}','{customer_email}')")
-
-#to see the changes in the sql server we have to use commit
-conn.commit()
-'''
-#-----'/' for the home page we use forward slash
+print(idgenerator('CUSTOMER'))
 @app.route('/')   #routing the homepage
 def home():
     return render_template('index.html')
 
 @app.route('/show-customers')
 def customer_show():
+    conn = sqlite3.connect('ims.db')
     cn = conn.cursor()
     cn.execute("select * from customer")
     data = []
@@ -50,6 +52,7 @@ def customer_show():
 
 @app.route('/show-products')
 def product_show():
+    conn = sqlite3.connect('ims.db')
     cn = conn.cursor()
     cn.execute("select * from product")
     data = []
@@ -67,6 +70,7 @@ def product_show():
 
 @app.route('/show-supplier')
 def supplier_show():
+    conn = sqlite3.connect('ims.db')
     cn = conn.cursor()
     cn.execute("select * from supplier")
     data = []
@@ -82,6 +86,7 @@ def supplier_show():
 
 @app.route('/show-orders')
 def orders_show():
+    conn = sqlite3.connect('ims.db')
     cn = conn.cursor()
     cn.execute("select * from orders")
     data = []
@@ -99,11 +104,13 @@ def orders_show():
 @app.route('/add-customer',methods = ['GET','POST'])
 def customer_add():
     if request.method == 'POST':
+        conn= sqlite3.connect('ims.db')
         cn = conn.cursor()
         customer_name = request.form.get("name")   #from the addcustomers html file ID = "name"
         customer_address = request.form.get("address")
         customer_email = request.form.get("email")
-        cn.execute(f"insert into customer(customer_name,customer_address,customer_email) values ('{customer_name}','{customer_address}','{customer_email}')")
+        ID = idgenerator('CUSTOMER')
+        cn.execute(f"insert into customer(customer_id,customer_name,customer_address,customer_email) values ('{ID}','{customer_name}','{customer_address}','{customer_email}')")
         conn.commit()
         print('Data has been Inserted')
         return jsonify({'message':'successful'})
@@ -113,6 +120,7 @@ def customer_add():
 @app.route('/update-customer',methods = ['GET','POST'])
 def customer_update():
     if request.method == 'POST':
+        conn = sqlite3.connect('ims.db')
         cn = conn.cursor()
         customer_id = request.form.get("customer_id")   #from the addcustomers html file ID = "name"
         change = request.form.get("old_value")
@@ -128,6 +136,7 @@ def customer_update():
 def customer_delete():
     if request.method == 'POST':
         customer_id = request.form.get("customer_id")
+        conn = sqlite3.connect('ims.db')
         cn = conn.cursor()
 
         try:
@@ -148,6 +157,7 @@ def customer_delete():
 @app.route('/add-product',methods = ['GET','POST'])
 def product_add():
     if request.method == 'POST':
+        conn = sqlite3.connect('ims.db')
         cn = conn.cursor()
         product_name = request.form.get("product_name")
         stock = request.form.get("stock")
@@ -163,6 +173,7 @@ def product_add():
 @app.route('/add-orders',methods=['GET','POST'])
 def orders_add():
     if request.method == 'POST':
+         conn = sqlite3.connect('ims.db')
          cn = conn.cursor()
          product_id=request.form.get('product_id')
          customer_id=request.form.get('customer_id')
@@ -177,6 +188,7 @@ def orders_add():
 @app.route('/add-supplier',methods=['GET','POST'])
 def supplier_add():
     if request.method == 'POST':
+         conn = sqlite3.connect('ims.db')
          cn = conn.cursor()
          supplier_name=request.form.get('supplier_name')
          supplier_address=request.form.get('supplier_address')
@@ -191,6 +203,7 @@ def supplier_add():
 @app.route('/update-product',methods=['GET','POST'])
 def product_update():
     if request.method == 'POST':
+         conn = sqlite3.connect('ims.db')
          cn = conn.cursor()
          product_id=request.form.get("product_id")
          change=request.form.get("change")
@@ -205,6 +218,7 @@ def product_update():
 @app.route('/update-orders',methods=['GET','POST'])
 def orders_update():
     if request.method == 'POST':
+         conn = sqlite3.connect('ims.db')
          cn = conn.cursor()
          order_id=request.form.get("order_id")
          change=request.form.get("change")
@@ -219,6 +233,7 @@ def orders_update():
 @app.route('/update-supplier',methods=['GET','POST'])
 def supplier_update():
     if request.method == 'POST':
+         conn = sqlite3.connect('ims.db')
          cn = conn.cursor()
          supplier_id=request.form.get("supplier_id")
          change=request.form.get("change")
@@ -234,6 +249,7 @@ def supplier_update():
 def product_delete():
     if request.method == 'POST':
         product_id = request.form.get("product_id")
+        conn = sqlite3.connect('ims.db')
         cn = conn.cursor()
 
         try:
@@ -253,6 +269,7 @@ def product_delete():
 def orders_delete():
     if request.method == 'POST':
         order_id = request.form.get("order_id")
+        conn = sqlite3.connect('ims.db')
         cn = conn.cursor()
 
         try:
@@ -271,6 +288,7 @@ def orders_delete():
 def supplier_delete():
     if request.method == 'POST':
         supplier_id = request.form.get("supplier_id")
+        conn = sqlite3.connect('ims.db')
         cn = conn.cursor()
 
         try:
@@ -293,7 +311,4 @@ def supplier_delete():
         return render_template('deletesupplier.html')
 
 if __name__ == '__main__':
-    app.run()
-
-
-'''host = '0.0.0.0', port = 5000, debug = True'''
+    app.run(host = '0.0.0.0', port = 5000, debug = False)
--- new.py
+++ new.py
@@ -1,8 +1,10 @@
 import sqlite3
-
-conn = sqlite3.connect('mynewsql.db')
+conn = sqlite3.connect('ims.db')
 cur = conn.cursor()
-#cur.execute('create table student(sid int, sname varchar(20))')
-cur.execute('insert into student values (1,"raman")')
+# cur.execute("CREATE TABLE CUSTOMER(CUSTOMER_ID VARCHAR(30), CUSTOMER_NAME VARCHAR(40), CUSTOMER_ADDRESS VARCHAR(30), CUSTOMER_EMAIL VARCHAR(40))")
+# cur.execute('CREATE TABLE SUPPLIER(SUPPLIER_ID VARCHAR(30),SUPPLIER_NAME VARCHAR(40),SUPPLIER_ADDRESS VARCHAR(30),SUPPLIER_EMAIL VARCHAR(40))')
+# cur.execute('CREATE TABLE PRODUCT(PRODUCT_ID VARCHAR(30),PRODUCT_NAME VARCHAR(40),STOCK INT,PRICE FLOAT,SUPPLIER_ID VARCHAR(30))')
+# cur.execute('CREATE TABLE ORDERS(ORDER_ID VARCHAR(30),PRODUCT_ID VARCHAR(30),CUSTOMER_ID VARCHAR(30),QUANTITY INT)')
+cur.execute("insert into CUSTOMER values('cus1','raman','nzb','raman@gmail.com')")
 conn.commit()
 
